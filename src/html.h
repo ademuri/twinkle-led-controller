cc9bceeb242ce576851673726a3ffd558c1f5485
Adam Demuri <adam.demuri@gmail.com>
2020-08-31 19:25:58 -0600

    Add initial software - just a basic web server

---
+++ src/html.h
@@ -0,0 +1,23 @@
+#ifndef HTML_H
+#define HTML_H
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {
+     font-family: Arial;
+     font-size: 3.0rem;
+     background-color: black;
+     color: white;
+    }
+  </style>
+</head>
+<body>
+  <h3>Twinkle Controller</h3>
+</body>
+</html>)rawliteral";
+
+#endif
+
