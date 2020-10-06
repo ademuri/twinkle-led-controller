b9a9ff9791d7905d28083e5c9ca86efcfff1b7ff
Adam Demuri <adam.demuri@gmail.com>
2020-10-05 22:38:18 -0600

    Disable the PWM interrupt during OTA (fixes OTA)

--- src/twinkle.ino
+++ src/twinkle.ino
@@ -119,6 +119,9 @@ void setup() {
 
   ArduinoOTA
     .onStart([]() {
+      // This interrupt interferes with the update process
+      timerAlarmDisable(timer);
+
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH) {
         type = "sketch";
@@ -138,6 +141,8 @@ void setup() {
     });
   ArduinoOTA
     .onError([](ota_error_t error) {
+      timerAlarmEnable(timer);
+
       Serial.printf("Error[%u]: ", error);
       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
